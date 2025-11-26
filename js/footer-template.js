4fdb1f1c249ef33fc731c0603ade8336ae1006e8
KadekArie <119066190+KadekArie@users.noreply.github.com>
2025-11-26 15:22:20 +0800

    Update footer-template.js

--- js/footer-template.js
+++ js/footer-template.js
@@ -1,7 +1,7 @@
 function getBasePath() {
     const pathSegments = window.location.pathname.split('/').filter(segment => segment.length > 0);
     if (pathSegments.length > 1 && pathSegments[pathSegments.length - 1].endsWith('.html')) {
-        return '../';
+        return './';
     } else if (pathSegments.length >= 1 && pathSegments[pathSegments.length - 1] !== 'index.html' && pathSegments[pathSegments.length - 1] !== '') {
         return './';
     } else {
